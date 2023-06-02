bc55679253e3ae6a5c5717e95dfeb18be81bbcc0
Yaroslav Kulpan <yaroslavkulpan@Yaroslavs-MacBook-Air.local>
2023-06-02 18:23:57 +0300

    feat(CMUSY-10): Fix imports

--- libs/cmusy-ui/src/lib/table/table-header.tsx
+++ libs/cmusy-ui/src/lib/table/table-header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Checkbox } from '../checkbox';
+import Checkbox from '../checkbox';
 import { TableColumn } from './table-column';
 import { useTable, useTableAction } from './context/table.context';
 import { tableHeader, TableHeaderVariants } from './table.theme';
